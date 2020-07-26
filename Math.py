8ea03b524babcfe6e2e9401622a2bdd0768c7543
Arvind Mukund <MasterAwesome@users.noreply.github.com>
2020-07-25 23:37:28 -0400

    [FIX] Turns out some values were wrong.
    This increases the multiplier for the CS_SIEGE_MAGIC_NUMBER_AIM_DOWN which was earlier 0.6 which caused a slower aim down than the awp in csgo. This is fixed with this patch.

--- Math.py
+++ Math.py
@@ -15,12 +15,18 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 def convert_cs_to_siege(hipfire, awp):
-    siege_hip_multiplier = hipfire * 0.00153589608577778
-    siege_awp_multiplier = awp * 0.0126984126984127
+    SIEGE_CS_MAGIC_NUMBER_LOOK_AROUND = 0.00153589608577778
+    SIEGE_CS_MAGIC_NUMBER_AIM_DOWN = 0.0126984126984127
+    
+    siege_hip_multiplier = hipfire * SIEGE_CS_MAGIC_NUMBER_LOOK_AROUND
+    siege_awp_multiplier = awp * SIEGE_CS_MAGIC_NUMBER_AIM_DOWN
     return [str(round(siege_hip_multiplier, 6)), str(round(siege_awp_multiplier, 6))]
 
 
 def convert_siege_to_cs(sens, mmultipler, aim, amultiplier):
-    cs_hip = 13.02171428470 * sens * mmultipler
-    cs_awp = amultiplier * 50  * aim * 0.012
+    CS_SIEGE_MAGIC_NUMBER_LOOK_AROUND = 13.02171428470
+    CS_SIEGE_MAGIC_NUMBER_AIM_DOWN = 0.7875
+    
+    cs_hip =  sens * mmultipler * CS_SIEGE_MAGIC_NUMBER_LOOK_AROUND
+    cs_awp = amultiplier * aim * CS_SIEGE_MAGIC_NUMBER_AIM_DOWN
     return [str(round(cs_hip, 2)), str(round(cs_awp, 2))]
